935a1f0c2867a8b4d2a0c49ef52db74b8a400ede
JustANipple <prandisamuel99@gmail.com>
2024-12-03 12:15:48 +0100

    Added modal to confirm comment deletion

--- lib/components/comments/comment/card/footer/delete_button.dart
+++ lib/components/comments/comment/card/footer/delete_button.dart
@@ -22,7 +22,7 @@ class DeleteButton extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
           ),
           onPressed: () {
-            commentProvider.deleteCommentById(comment.id!);
+            showModal(context, commentProvider, comment.id!);
           },
           child: Row(
             children: [
@@ -47,4 +47,92 @@ class DeleteButton extends StatelessWidget {
       },
     );
   }
+}
+
+void showModal(BuildContext context, CommentProvider commentProvider, int id) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        insetPadding: EdgeInsets.symmetric(horizontal: 16),
+        title: Text(
+          'Delete comment',
+          style: GoogleFonts.rubik(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: Color.fromRGBO(51, 66, 83, 1)),
+        ),
+        content: Container(
+          constraints: BoxConstraints(
+            maxWidth: 342,
+          ),
+          child: Text(
+            'Are you sure you want to delete this comment? This will remove the comment and can'
+            't be undone.',
+            style: GoogleFonts.rubik(
+              fontSize: 16,
+              fontWeight: FontWeight.normal,
+              height: 1.5,
+              color: Color.fromRGBO(103, 114, 126, 1),
+            ),
+          ),
+        ),
+        actions: [
+          Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(103, 114, 126, 1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  ),
+                  child: Text(
+                    "NO, CANCEL",
+                    style: GoogleFonts.rubik(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        height: 1.5,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+              SizedBox(width: 12),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    commentProvider.deleteCommentById(id);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(237, 99, 104, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  ),
+                  child: Text(
+                    "YES, DELETE",
+                    style: GoogleFonts.rubik(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        height: 1.5,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      );
+    },
+  );
 }
--- lib/components/comments/comment/card/header.dart
+++ lib/components/comments/comment/card/header.dart
@@ -40,60 +40,63 @@ class _CommentCardHeaderState extends State<CommentCardHeader> {
   Widget build(BuildContext context) {
     return Consumer<CommentProvider>(
         builder: (context, commentProvider, child) {
-      return Row(
-        children: [
-          Image(
-            image: AssetImage(widget.user.avatarImageUrl),
-            width: 32,
-            height: 32,
-          ),
-          SizedBox(width: 16),
-          Row(
-            children: [
-              Text(
-                widget.user.name,
-                style: GoogleFonts.rubik(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: Color.fromRGBO(51, 63, 83, 1),
-                ),
-              ),
-              SizedBox(width: 8),
-              if (commentProvider.userAddId == widget.comment.userAddId)
-                Container(
-                  padding: EdgeInsets.fromLTRB(6, 2, 6, 4),
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(83, 87, 182, 1),
-                    borderRadius: BorderRadius.circular(2),
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        "you",
-                        style: GoogleFonts.rubik(
-                          fontSize: 13,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white,
-                          height: 1,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-            ],
-          ),
-          SizedBox(width: 16),
-          Text(
-            CommentProvider.calculatePastTime(widget.comment),
-            style: GoogleFonts.rubik(
-              fontWeight: FontWeight.normal,
-              fontSize: 16,
-              height: 2.4,
-              color: Color.fromRGBO(103, 114, 126, 1),
+      return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            Image(
+              image: AssetImage(widget.user.avatarImageUrl),
+              width: 32,
+              height: 32,
             ),
-          ),
-        ],
+            SizedBox(width: 16),
+            Row(
+              children: [
+                Text(
+                  widget.user.name,
+                  style: GoogleFonts.rubik(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Color.fromRGBO(51, 63, 83, 1),
+                  ),
+                ),
+                SizedBox(width: 8),
+                if (commentProvider.userAddId == widget.comment.userAddId)
+                  Container(
+                    padding: EdgeInsets.fromLTRB(6, 2, 6, 4),
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(83, 87, 182, 1),
+                      borderRadius: BorderRadius.circular(2),
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "you",
+                          style: GoogleFonts.rubik(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                            height: 1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+              ],
+            ),
+            SizedBox(width: 16),
+            Text(
+              CommentProvider.calculatePastTime(widget.comment),
+              style: GoogleFonts.rubik(
+                fontWeight: FontWeight.normal,
+                fontSize: 16,
+                height: 2.4,
+                color: Color.fromRGBO(103, 114, 126, 1),
+              ),
+            ),
+          ],
+        ),
       );
     });
   }
