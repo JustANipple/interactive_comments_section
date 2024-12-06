bb0fa29e040abf56a0529335348b0e81595997ce
JustANipple <prandisamuel99@gmail.com>
2024-12-06 10:01:57 +0100

    Hosted on firebase, added README.md

---
+++ .firebase/interactive-comments-6768e/hosting/flutter_service_worker.js
@@ -0,0 +1,222 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"assets/AssetManifest.bin": "0bd955924e2af30e69a3f3b3079a2e8d",
+"assets/AssetManifest.bin.json": "070fe2416e61d9e09c67c612a6a770b0",
+"assets/AssetManifest.json": "9a64f9353fc91608c767be8fb3ab47ce",
+"assets/assets/avatars/image-amyrobson.png": "9af1cb34e84f3da0066a4ca72fd0c52b",
+"assets/assets/avatars/image-amyrobson.webp": "afcbc55dc0c24a28e7488297e2ae37c9",
+"assets/assets/avatars/image-juliusomo.png": "89f2bdb2e6d14539202d953ab6446739",
+"assets/assets/avatars/image-juliusomo.webp": "d284b5c52de5e301648a265fdf17e896",
+"assets/assets/avatars/image-maxblagun.png": "fa736fb06e3bec49ca41c1947b1ff56b",
+"assets/assets/avatars/image-maxblagun.webp": "41a4e8fad378bc27a036319ed50df7cd",
+"assets/assets/avatars/image-ramsesmiron.png": "2c5308fc3f8404d830d949b4ddcec8e6",
+"assets/assets/avatars/image-ramsesmiron.webp": "eaf07ed620549a7dfcb54ed1ee06f204",
+"assets/assets/favicon-32x32.png": "a07f423b8bbf4a50c2c449b96795fcb6",
+"assets/assets/icon-delete.svg": "d44a925a9b6f04ef202b275c66ec4ad9",
+"assets/assets/icon-edit.svg": "88e96924661e581748e92d0bd749a0c6",
+"assets/assets/icon-minus.svg": "c420fc9225de671c939c9e2f83fefea1",
+"assets/assets/icon-plus.svg": "2736214f8c412e592595d18af82f7615",
+"assets/assets/icon-reply.svg": "cb6fbfc8621c00257f07b1706698767c",
+"assets/assets/mobile_solution.png": "61d23cafef79f0280c77de1f4e374717",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/fonts/MaterialIcons-Regular.otf": "60e226c19a79769bd9e7ba96811948a7",
+"assets/NOTICES": "5b4297290378395077b7a65c6bc2444d",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
+"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
+"canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
+"canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
+"canvaskit/canvaskit.wasm": "1f237a213d7370cf95f443d896176460",
+"canvaskit/chromium/canvaskit.js": "671c6b4f8fcc199dcc551c7bb125f239",
+"canvaskit/chromium/canvaskit.js.symbols": "a012ed99ccba193cf96bb2643003f6fc",
+"canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
+"canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
+"canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
+"canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
+"canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "f393d3c16b631f36852323de8e583132",
+"flutter_bootstrap.js": "5c765d790c701f84eb8afa06983e2317",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"index.html": "5cc4e83bf1823304df686e9bd93e3e6b",
+"/": "5cc4e83bf1823304df686e9bd93e3e6b",
+"main.dart.js": "5e426470a2fd3a5d54a1c934f8e4d17a",
+"manifest.json": "04faf648c3aa827e8b9f59371a83cb58",
+"sqflite_sw.js": "4e90ada89670da328d6c72679eb3f9a7",
+"sqlite3.wasm": "fa7637a49a0e434f2a98f9981856d118",
+"version.json": "5d8a7eefe08552bb4b05d10dd8ea0890"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"flutter_bootstrap.js",
+"assets/AssetManifest.bin.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
--- README.md
+++ README.md
@@ -1,11 +1,79 @@
-# interactive_comments_section
+# Frontend Mentor - Interactive comments section solution
 
-Each record in DB must have:
+This is a solution to
+the [Interactive comments section challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-comments-section-iG1RugEG9).
+Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
-- Comment text
-- User ID that added comment
-- User ID that updated comment
-- Date of creation
-- Date of update
-- Number of likes
-- Replies (Comment type)
+## Table of contents
+
+- [Overview](#overview)
+    - [The challenge](#the-challenge)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+- [My process](#my-process)
+    - [Built with](#built-with)
+    - [What I learned](#what-i-learned)
+    - [Continued development](#continued-development)
+    - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Create, Read, Update, and Delete comments and replies
+- Upvote and downvote comments
+- Keeping state of the site with SQLite database
+- Change the user that's currently logged in by clicking the bottom left avatar
+
+### Screenshot
+
+![](./assets/mobile_solution.png)
+
+### Links
+
+- Solution URL: [GitHub](https://github.com/JustANipple/interactive_comments_section)
+- Live Site URL: [FireBae](https://interactive-comments-6768e.web.app/)
+
+## My process
+
+### Built with
+
+- Flutter
+- Dart
+- Firebase
+- SQLite
+- Mobile-first workflow
+
+### What I learned
+
+This has been the first time i built a full stack app in Flutter, so it was quite challenging
+SQLite was a good choice for this challenge as it's a simple database that can be used to store
+comments and replies.
+Flutter has a nice way to manage state, which is really useful when you need to update UI after an
+action is done.
+
+### Continued development
+
+I will continue studying on layout for the Flutter part and structure for the backend (Dart)
+
+### Useful resources
+
+- [SQLite getting started](https://www.sqlitetutorial.net/sqlite-getting-started/) - This was a good
+  resource to setup SQLite for the first time.
+- [Flutter docs](https://docs.flutter.dev/) - Flutter has nice documentation for everything you need
+  to know.
+
+## Author
+
+- Frontend Mentor - [@JustANipple](https://www.frontendmentor.io/profile/JustANipple)
+
+## Acknowledgments
+
+Since frontendmentor works with websites and not with app, as a screen result on that site for this
+challenge i couldn't get anything other than a blank screen, but VillageR88 gave me a hint to
+generate themed loading screen that prevents this to happen, so i added it to the project.
+- VillageR88 - [@VillageR88](https://github.com/VillageR88)
